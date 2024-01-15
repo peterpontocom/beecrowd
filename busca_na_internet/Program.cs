3089163df32b05ac01f38a8b896b406205e5bffd
peterpontocom <willpontocom07@gmail.com>
2024-01-15 12:09:06 +0100

    two new algorithms solved

---
+++ busca_na_internet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int casosTeste = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < casosTeste; i++)
+            {
+                int P3 = int.Parse(Console.ReadLine());
+
+                int P2 = 2 * P3;
+                int P1 = 2 * P2;
+
+                Console.WriteLine(P1);
+            }
+        }
+    }
+
+}
---
+++ identificando_cha/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int tipoReal = int.Parse(Console.ReadLine());
+
+            string[] respostasStr = Console.ReadLine().Split(' ');
+            int[] respostas = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                respostas[i] = int.Parse(respostasStr[i]);
+            }
+
+            int corretas = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (respostas[i] == tipoReal)
+                {
+                    corretas++;
+                }
+            }
+
+            Console.WriteLine(corretas);
+        }
+    }
+}
